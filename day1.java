30f897ba67abe4c256ff68436e46c17510fa8f69
Kalaiselvi245 <kalaiselvi02042005@gmail.com>
2025-06-17 14:41:40 +0530

    Update day1.java

--- day1.java
+++ day1.java
@@ -1,14 +1,4 @@
- import java.util.Scanner;
 
-public class ReverseArray {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-       
-        System.out.print("Enter number of elements: ");
-        int n = scanner.nextInt();
-
-        int[] numbers = new int[n];
 task 1
        
         System.out.println("Enter the numbers:");
